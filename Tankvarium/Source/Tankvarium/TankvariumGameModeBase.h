1db0d5a2cdb785dcd11c15a2dfbb9b4197e122c2
Mihail Kirov <shimonchick@gmail.com>
2018-07-31 00:48:06 +0300

    tank game landscape , tank, started UI

---
+++ Tankvarium/Source/Tankvarium/TankvariumGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankvariumGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKVARIUM_API ATankvariumGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
